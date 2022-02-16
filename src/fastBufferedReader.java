f49360e89eaf4a14bc9a87d0adaef61ae7d096b4
nyannnnn <90939473+nyannnnn@users.noreply.github.com>
2022-02-16 14:45:08 -0500

    yes

---
+++ src/fastBufferedReader.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+public class fastBufferedReader {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer in;
+	
+	public static void main(String[] args) throws IOException {
+		
+	}
+	
+	static String next() throws IOException {
+		while (in == null || !in.hasMoreTokens())
+			in = new StringTokenizer(br.readLine());
+		return in.nextToken();
+	}
+
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readChar() throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine() throws IOException {
+		return br.readLine();
+	}
+}
